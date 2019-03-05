3e2d70df824b1833493912eac396b12a0e1bd4f5
DanielSkill <danskillington@gmail.com>
2019-03-05 14:57:36 +0000

    add created at and updated timestamps to seeder

--- database/seeds/AchievementsTableSeeder.php
+++ database/seeds/AchievementsTableSeeder.php
@@ -13,10 +13,10 @@ class AchievementsTableSeeder extends Seeder
     public function run()
     {
         Achievement::insert([
-            ['name' => 'Highest Overall XP Day', 'skill' => 'Overall', 'timespan' => 1],
-            ['name' => 'Highest Overall XP Week', 'skill' => 'Overall', 'timespan' => 7],
-            ['name' => 'Highest Overall XP Month', 'skill' => 'Overall', 'timespan' => 30],
-            ['name' => 'Highest Overall XP Year', 'skill' => 'Overall', 'timespan' => 365]
+            ['name' => 'Highest Overall XP Day', 'skill' => 'Overall', 'timespan' => 1, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Highest Overall XP Week', 'skill' => 'Overall', 'timespan' => 7, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Highest Overall XP Month', 'skill' => 'Overall', 'timespan' => 30, 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'Highest Overall XP Year', 'skill' => 'Overall', 'timespan' => 365, 'created_at' => now(), 'updated_at' => now()]
         ]);
     }
 }
